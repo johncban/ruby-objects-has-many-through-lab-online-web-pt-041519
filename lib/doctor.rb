824f65634c29fdd39a0e073ad140bf5d501e1b42
Juan Carlo Banayo <johncban@gmail.com>
2019-05-26 19:02:39 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,29 @@
+class Doctor
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(docname)
+    @name = docname
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment(patient, date)
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+  def patients
+    appointment.map(&:patient) # https://stackoverflow.com/questions/9429819/what-is-the-functionality-of-operator-in-ruby
+  end
+
+end
---
+++ lib/patient.rb
@@ -0,0 +1,28 @@
+class Patient
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(p_name)
+    @name = p_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_appointment
+    Appointment.new(date, self, doctor)
+  end
+
+  def appointments
+    Appointment.all do |apt|
+      apt.new == self
+    end
+  end
+
+  def doctors
+    appointments.map(&:doctor)
+  end
+end
